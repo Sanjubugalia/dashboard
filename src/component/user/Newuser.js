568370c7693853aaad86321bee5894c02afa9bf7
Pooja Bugalia <sanjubugaliag@gmail.com>
2022-06-03 12:21:39 +0530

    first commit

--- src/component/user/Newuser.js
+++ src/component/user/Newuser.js
@@ -1,44 +1,64 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "../user/user.css"
+import axios from 'axios'
+
 
 function Newuser() {
-  return (
-  <>
-  <div className='container'>
-  <div className="newusersm">
-          <h4 className="widgettitle"> New User </h4>
-          
-          <div className="container">
-  <lable>Name</lable><br/>
-   <input type="text" className="inpnewuser" placeholder="Name"/><br/>
-   <lable>Password</lable><br/>
-  <input type="text" className="inpnewuser ml-5" placeholder="Password"/><br/>
-   <lable>Phone No.:</lable><br/>
-   <input type="text" className="inpnewuser" placeholder="Phone Number"/><br/>
-    <lable>Address:</lable><br/>
-    <input type="text" className="inpnewuser" placeholder="Address"/><br/>
-    <button class="btn btn-primary" type="button">Button</button>
-</div>
-          </div>
+const [inpdata, setInpdata] = useState([{username:"", password:"", email:"", phonenum:"",fullname:"", gender:"", address:"", status:""}])
+const {username, password, email, phonenum, fullname, gender,address,status}=inpdata
 
-  <div className="newuserlg float-right">
-        <h4 className="widgettitle"></h4>
-        <div className='widgetmember hrclass'>
-        <lable>Full Name</lable><br/>
-   <input type="text" className="inpnewuser" placeholder="Full Name"/><br/>
-   <lable>Gender</lable><br/>
-   <input type="text" className="inpnewuser" placeholder="Gender"/><br/>
-        <lable>Email:</lable><br/>
-  <input type="text" className="inpnewuser ml-5" placeholder="name@example.com"/><br/>
-   <lable>Active:</lable><br/>
-    <input type="text" className="inpnewuser" placeholder="active"/><br/>
-            </div>
-            </div>
-            </div>
 
-  
-  </>
-  )
+const inputHandler=(e)=>{
+setInpdata({...inpdata,[e.target.name]:e.target.value})
+console.log({[e.target.name]:e.target.value})
+}
+
+const btnclick= async()=>{
+        await axios.post("http://localhost:3003/posts");
+}
+
+useEffect(()=>{
+        btnclick()
+})
+
+        return (
+                <>
+                  
+                        <div className='container'>
+                                <div className="newusersm">
+                                        <h4 className="widgettitle"> New User </h4>
+                                        <div className="container">
+                                                <label>Name</label><br />
+                                                <input type="text" className="inpnewuser" name="username" value={username} onChange={(e)=>{inputHandler(e)}} placeholder="Name" /><br />
+                                                <label>Password</label><br />
+                                                <input type="text" className="inpnewuser ml-5" name="password" value={password} onChange={(e)=>{inputHandler(e)}} placeholder="Password" /><br />
+                                                <label>Phone No.:</label><br />
+                                                <input type="text" className="inpnewuser" name="phonenum" value={phonenum} onChange={(e)=>{inputHandler(e)}} placeholder="Phone Number" /><br />
+                                                <label>Address:</label><br />
+                                                <input type="text" className="inpnewuser" name="address" value={address} onChange={(e)=>{inputHandler(e)}} placeholder="Address" /><br />
+
+                                        </div>
+                                </div>
+
+                                <div className="newuserlg float-right">
+                                        <h4 className="widgettitle"></h4>
+                                        <div className='widgetmember hrclass'>
+                                                <label>Full Name</label><br />
+                                                <input type="text" className="inpnewuser" name="fullname" value={fullname} onChange={(e)=>{inputHandler(e)}} placeholder="Full Name" /><br />
+                                                <label>Gender</label><br />
+                                                <input type="text" className="inpnewuser" name="gender" value={gender} onChange={(e)=>{inputHandler(e)}} placeholder="Gender" /><br />
+                                                <label>Email:</label><br />
+                                                <input type="text" className="inpnewuser ml-5" name="email" value={email} onChange={(e)=>{inputHandler(e)}} placeholder="name@example.com" /><br />
+                                                <label>Status:</label><br />
+                                                <input type="text" className="inpnewuser" name="status" value={status} onChange={(e)=>{inputHandler(e)}} placeholder="active" /><br />
+                                                <button className="btn btn-primary" type="button" onClick={()=>btnclick()}>Button</button>
+                                        </div>
+                                </div>
+                        </div>
+
+                     
+                </>
+        )
 }
 
 export default Newuser
